fa67eccfffac59248242e9fdf7c623028cf74477
Andrii Dubchak <andrii.dubchak@magnise.com>
2019-08-05 22:48:59 +0300

    remove visualizations

--- visualizationLibrary/projects/visualizations/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of visualizations
- */
-
-export * from './lib/visualizations.service';
-export * from './lib/visualizations.component';
-export * from './lib/visualizations.module';
